bb3d26bf1c2066b75f6efd065f5350ff72e727e4
whit <armstrong.whit@gmail.com>
2010-10-12 16:30:25 -0400

    update to Normal to use schur product when both mu and tau are mat objects

--- cppbugs/mcmc.normal.hpp
+++ cppbugs/mcmc.normal.hpp
@@ -30,7 +30,12 @@ namespace cppbugs {
 
     template<typename U, typename V>
     double logp(const U& mu, const V& tau) const {
-      return accu(0.5*log(0.5*tau/arma::math::pi()) - 0.5 * tau * pow(Stochastic<T>::value - mu,2));
+      return accu(0.5*log(0.5*tau/arma::math::pi()) - 0.5 * tau * pow(Stochastic<T>::value - mu,2.0));
+    }
+
+    // need this specialization b/c we need to do schur product btwn two mat's
+    double logp(const arma::mat& mu, const arma::mat& tau) const {
+      return accu(0.5*log(0.5*tau/arma::math::pi()) - 0.5 * tau % pow(Stochastic<T>::value - mu,2.0));
     }
   };
 
@@ -40,7 +45,7 @@ namespace cppbugs {
   public:
     NormalStatic(const T& x, const double mu, const double tau, const bool observed = false): Stochastic<T>(x,observed), mu_(mu), tau_(tau) {}
     double logp() const {
-      return accu(0.5*log(0.5*tau_/arma::math::pi()) - 0.5 * tau_ * pow(Stochastic<T>::value - mu_,2));
+      return accu(0.5*log(0.5*tau_/arma::math::pi()) - 0.5 * tau_ * pow(Stochastic<T>::value - mu_,2.0));
     }
   };
 
