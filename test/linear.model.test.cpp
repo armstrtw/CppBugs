5eae370fdfd7c096e9dfe3411b87d35c345f3671
whit <armstrong.whit@gmail.com>
2012-01-18 17:11:03 -0500

    replace linear.model.test.cpp -- actually do it

--- test/linear.model.test.cpp
+++ test/linear.model.test.cpp
@@ -1,46 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <armadillo>
+#include <boost/random.hpp>
 #include <cppbugs/cppbugs.hpp>
+#include <cppbugs/mcmc.model.hpp>
 
 using namespace arma;
 using namespace cppbugs;
 using std::cout;
 using std::endl;
-
-class TestModel: public MCModel {
-public:
-  const mat& y; // given
-  const mat& X; // given
-
-  Normal<vec> b;
-  Uniform<double> tau_y;
-  Deterministic<mat> y_hat;
-  Normal<mat> likelihood;
-  Deterministic<double> rsq;
-
-  TestModel(const mat& y_,const mat& X_): y(y_), X(X_),
-                                          b(randn<vec>(X_.n_cols)),
-                                          tau_y(1),
-                                          y_hat(X*b.value),
-                                          likelihood(y_,true),
-                                          rsq(0)
-  {
-    add(b);
-    add(tau_y);
-    add(y_hat);
-    add(likelihood);
-    add(rsq);
-  }
-
-  void update() {
-    y_hat.value = X*b.value;
-    rsq.value = as_scalar(1 - var(y - y_hat.value) / var(y));
-    b.dnorm(0.0, 0.0001);
-    tau_y.dunif(0,100);
-    likelihood.dnorm(y_hat.value,tau_y.value);
-  }
-};
 
 int main() {
   const int NR = 1e2;
@@ -54,17 +22,34 @@ int main() {
   solve(coefs, X, y);
   vec err = y - X*coefs;
 
-  TestModel m(y,X);
+  vec b = randn<vec>(2);
+  mat y_hat;
+  double rsq(0);
+  double tau_y(1);
+
+  std::function<void ()> model = [&]() {
+    y_hat = X * b;
+    rsq = as_scalar(1 - var(y - y_hat) / var(y));
+  };
+
+  MCModel m(model);
+
+  m.normal(b).dnorm(0.0, 0.0001);
+  m.uniform(tau_y).dunif(0,100);
+  m.normal(y).dnorm(y_hat,tau_y);
+  m.deterministic(rsq);
+
+
   int iterations = 1e5;
   m.sample(iterations, 1e4, 1e4, 10);
-
   cout << "lm coefs" << endl << coefs;
   cout << "err sd: " << stddev(err,0) << endl;;
   cout << "err tau: " << pow(stddev(err,0),-2) << endl;
-  cout << "b: " << endl << m.b.mean();
-  cout << "tau_y: " << m.tau_y.mean() << endl;
-  cout << "R^2: " << m.rsq.mean() << endl;
-  cout << "samples: " << m.b.history.size() << endl;
+  cout << "b: " << endl << m.getNode(b).mean();
+  cout << "tau_y: " << m.getNode(tau_y).mean() << endl;
+  cout << "R^2: " << m.getNode(rsq).mean() << endl;
+  cout << "samples: " << m.getNode(b).history.size() << endl;
   cout << "acceptance_ratio: " << m.acceptance_ratio() << endl;
+
   return 0;
 };
