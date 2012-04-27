a91c165a2590a1178885595632812fab5af3a9dc
whit <armstrong.whit@gmail.com>
2012-04-27 13:26:49 -0400

    clean up Bernoulli and add jumper for T=double

--- cppbugs/mcmc.bernoulli.hpp
+++ cppbugs/mcmc.bernoulli.hpp
@@ -55,18 +55,15 @@ namespace cppbugs {
       }
     }
 
-  public:
-    Bernoulli(T& value): DynamicStochastic<T>(value) {}
-
-    /* original
-    void jump(RngBase& rng) {
-      double jump_probability = 1.0 - pow(0.5,DynamicStochastic<T>::scale_);
-      arma::uvec flips = arma::find(arma::randu<arma::vec>(DynamicStochastic<T>::value.n_elem) < jump_probability);
-      for(unsigned int i = 0; i < flips.n_elem; i++) {
-        DynamicStochastic<T>::value[ flips[i] ] = DynamicStochastic<T>::value[ flips[i] ] ? 0 : 1;
+    void bernoulli_jump(RngBase& rng, double& value, const double scale) {
+      double jump_probability = 1.0 - pow(0.5,scale);
+      if(rng.uniform() < jump_probability) {
+        value = value ? 0 : 1;
       }
     }
-    */
+
+  public:
+    Bernoulli(T& value): DynamicStochastic<T>(value) {}
 
     void jump(RngBase& rng) {
       bernoulli_jump(rng, DynamicStochastic<T>::value, DynamicStochastic<T>::scale_);
@@ -90,32 +87,5 @@ namespace cppbugs {
       return *this;
     }
   };
-
-  // template<>
-  // class Bernoulli <arma::ivec> : public Stochastic<arma::ivec> {
-  // public:
-  //   Bernoulli(const arma::ivec& value, const bool observed=false): Stochastic<arma::ivec>(value,observed) {}
-
-  //   void jump(RngBase& rng) {
-  //     double jump_probability = 1.0 - pow(0.5,Stochastic<arma::ivec>::scale_);
-  //     arma::uvec flips = find(arma::randu<arma::vec>(Stochastic<arma::ivec>::value.n_elem) < jump_probability);
-  //     for(unsigned int i = 0; i < flips.n_elem; i++) {
-  //       Stochastic<arma::ivec>::value[ flips[i] ] = Stochastic<arma::ivec>::value[ flips[i] ] ? 0 : 1;
-  //     }
-  //   }
-
-  //   void dbern(const arma::vec& p) {
-  //     arma::uvec p_less_than_eq_zero = find(p <= 0,1);
-  //     arma::uvec p_greater_than_eq_1 = find(p >= 1,1);
-  //     arma::uvec value_less_than_zero = find(Stochastic<arma::ivec>::value < 0,1);
-  //     arma::uvec value_greater_than_n = find(Stochastic<arma::ivec>::value > 1,1);
-
-  //     if(p_less_than_eq_zero.n_elem || p_greater_than_eq_1.n_elem || value_less_than_zero.n_elem || value_greater_than_n.n_elem) {
-  //       Stochastic<arma::ivec>::logp_ = -std::numeric_limits<double>::infinity();
-  //     } else {
-  //       Stochastic<arma::ivec>::logp_ = accu(Stochastic<arma::ivec>::value % log(p) + (1-Stochastic<arma::ivec>::value) % log(1-p));
-  //     }
-  //   }
-  // };
 } // namespace cppbugs
 #endif // MCMC_BERNOULLI_HPP
