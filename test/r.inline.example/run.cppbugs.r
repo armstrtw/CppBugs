43f77c525303918c843592fcf474e40a9a8abbed
whit <armstrong.whit@gmail.com>
2013-08-28 10:36:16 -0400

    update r.inline.example for new cppbugs conventions

--- test/r.inline.example/run.cppbugs.r
+++ test/r.inline.example/run.cppbugs.r
@@ -11,7 +11,10 @@ cppbugs.plugin <- getPlugin("RcppArmadillo")
 cppbugs.plugin$env$PKG_CXXFLAGS <- "-std=c++0x"
 cppbugs.plugin$env$PKG_LIBS <- paste(cppbugs.plugin$env$PKG_LIBS,"-larmadillo")
 
-linear.model <- cxxfunction(signature(XR="numeric", yr="numeric",iterations="integer",burn="integer",adapt="integer",adapt_interval="integer",thin="integer"), includes="#include <cppbugs/cppbugs.hpp>",body=src,settings=cppbugs.plugin)
+##linear.model <- cxxfunction(signature(XR="numeric", yr="numeric",iterations="integer",burn="integer",adapt="integer",adapt_interval="integer",thin="integer"), includes="#include <cppbugs/cppbugs.hpp>",body=src,settings=cppbugs.plugin)
+includes <- c("#include <cppbugs/cppbugs.hpp>","#include <cppbugs/deterministics/mcmc.linear.hpp>","#include <cppbugs/deterministics/mcmc.rsquared.hpp>")
+
+linear.model <- cxxfunction(signature(XR="numeric", yr="numeric",iterations="integer",burn="integer",adapt="integer",adapt_interval="integer",thin="integer"), includes=includes,body=src,settings=cppbugs.plugin)
 
 
 NR <- 1000
@@ -21,7 +24,7 @@ b <- rnorm(NC + 1)
 b[1] <- 10
 y <- X %*% b + rnorm(NR)
 
-res <- linear.model(XR=X,yr=y,iterations=1e5L,burn=1e5L,adapt=1e3L,thin=5L)
+res <- linear.model(XR=X,yr=y,iterations=1e5L,burn=1e5L,adapt=1e3L,adapt_interval=100L,thin=5L)
 
 cat("actual vs estimated:\n")
 act.vs.est <- cbind(as.matrix(b),res$b)
--- test/r.inline.example/run.model.cpp
+++ test/r.inline.example/run.model.cpp
@@ -15,28 +15,23 @@ mat y_hat = X * b;
 double rsq(0);
 double tau_y(1);
 
-std::function<void ()> model = [&]() {
-  y_hat = X * b;
-  rsq = as_scalar(1 - var(y - y_hat) / var(y));
-};
-
-MCModel<boost::minstd_rand> m(model);
+MCModel<boost::minstd_rand> m;
 m.link<Normal>(b, 0.0, 0.0001);
 m.link<Gamma>(tau_y, 0.1, 0.1);
-m.link<Deterministic>(y_hat);
+m.link<Linear>(y_hat, X, b);
+m.link<Rsquared>(rsq,y,y_hat);
 m.link<ObservedNormal>(y, y_hat, tau_y);
-m.link<Deterministic>(rsq);
 
 // things to track
 std::vector<vec>& b_hist = m.track<std::vector>(b);
 std::vector<double>& rsq_hist = m.track<std::vector>(rsq);
 
 
-  m.tune(adapt_,adapt_interval_);
-  m.tune_global(adapt_,adapt_interval_);
-  m.burn(burn_);
-  m.sample(iterations_, thin_);
-m.sample(, , , );
+m.tune(adapt_,adapt_interval_);
+m.tune_global(adapt_,adapt_interval_);
+m.burn(burn_);
+m.sample(iterations_, thin_);
+
 
 return Rcpp::List::create(Rcpp::Named("b", mean(b_hist.begin(),b_hist.end())),
                           Rcpp::Named("rsq", mean(rsq_hist.begin(),rsq_hist.end())),
