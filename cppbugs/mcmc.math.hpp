0b0513e6c89a591d08544e8a66c9749ccc78ad2f
Jacques-Henri Jourdan <jacques-henri.jourdan@inria.fr>
2015-10-24 18:46:10 +0200

    Checks for positivity in exponential distribution.

--- cppbugs/mcmc.math.hpp
+++ cppbugs/mcmc.math.hpp
@@ -134,6 +134,8 @@ namespace cppbugs {
 
   template<typename T, typename U>
   double exponential_logp(const T& x, const U& lambda) {
+    if(!arma::all(arma::vectorise(x > 0)) || !arma::all(arma::vectorise(lambda > 0)))
+      return -std::numeric_limits<double>::infinity();
     return arma::accu(log_approx(lambda) - arma::schur(lambda, x));
   }
 
